f4bb5a760485efd76222443ef835f7ed0d2ca371
GaiaGD <gaia.digregorio@gmail.com>
2023-03-27 18:56:16 -0400

    hash

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import './planets.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <PlanetThemeContextProvider>
-    <HashRouter>
+    <HashRouter basename='/front-end-portfolio-23/'>
       <React.StrictMode>
         <ScrollToTop />
         <App />
